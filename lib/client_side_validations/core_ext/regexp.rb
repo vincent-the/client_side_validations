e37d4b9e0787e4bc1a29420fc948fb45a5885698
Vincent <vincent@vincent-thelang.de>
2014-04-25 18:17:48 +0200

    Fix format validate

--- lib/client_side_validations/core_ext/regexp.rb
+++ lib/client_side_validations/core_ext/regexp.rb
@@ -1,6 +1,6 @@
 class Regexp
   def as_json(options = nil)
-    Regexp.new inspect.sub('\\A','^').sub('\\Z','$').sub('\\z','$').sub(/^\//,'').sub(/\/[a-z]*$/,'').gsub(/\(\?#.+\)/, '').gsub(/\(\?-\w+:/,'(').gsub(/\s/,''), self.options & 5
+    (Regexp.new inspect.sub('\\A','^').sub('\\Z','$').sub('\\z','$').sub(/^\//,'').sub(/\/[a-z]*$/,'').gsub(/\(\?#.+\)/, '').gsub(/\(\?-\w+:/,'(').gsub(/\s/,''), self.options & 5).inspect
   end
 
   def to_json(options = nil)
